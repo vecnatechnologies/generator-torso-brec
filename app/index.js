479aaa82a4fa19381710f083a0d30789189b894a
kent <kentmw@gmail.com>
2015-05-07 22:58:27 -0400

    First push at setting up a scaffolding

---
+++ app/index.js
@@ -0,0 +1,31 @@
+var generators = require('yeoman-generator');
+var _ = require('underscore');
+
+var paths = {
+  'app/index.html': {},
+  'app/home/homeView.js': {},
+  'app/home/home-template.hbs': {},
+  'app/app.scss': {},
+  'app/main.js': {},
+  'dist/.keepme': {},
+  'package.json': {},
+  'gulpfile.js': {},
+  '.gitignore': {}
+};
+
+module.exports = generators.Base.extend({
+  writing: function() {
+    _.each(paths, function(context, path) {
+      console.log('copying: ' + path);
+      this.fs.copyTpl(
+          this.templatePath(path),
+          this.destinationPath(path),
+          context
+      );
+    }, this);
+  },
+
+  install: function () {
+    this.npmInstall();
+  }
+});
